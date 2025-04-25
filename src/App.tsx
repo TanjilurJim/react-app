95f60e4677de75789b2476f73f516c191720878b
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2025-04-25 23:54:28 +0600

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import Message from "./Message"; // import the Message component from Message.tsx file, it is a default export, so we can import it with any name
+import ListGroup from "./components/ListGroup";
 function App() {
-  return (
-    <div>
-      <Message />
-    </div>
-  );
+  return <div>ListGroup</div>;
 }
 
 export default App; // export default is used to export a single component, it can be imported in any file with any name, but only one default export is allowed in a file
