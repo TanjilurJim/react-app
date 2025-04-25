7ad159de32d4829f93bfa9bd6f2476998ea9f73d
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2025-04-25 23:59:06 +0600

    fixed listGroup Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import ListGroup from "./components/ListGroup";
 function App() {
-  return <div>ListGroup</div>;
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App; // export default is used to export a single component, it can be imported in any file with any name, but only one default export is allowed in a file
